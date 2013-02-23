64a3efce948fa0135c145a0bf33849e450d251ea
Anna Fowles-Winkler <afowleswinkler@gmail.com>
2013-02-22 22:31:08 -0700

    Adding updating files.

--- lib/kontti.rb
+++ lib/kontti.rb
@@ -1,5 +1,32 @@
 require "kontti/version"
 
+# From http://en.wikipedia.org/wiki/Language_game
+#
+# Add word 'kontti' after each word and apply the same conversion as in sananmuunnos.
+# Finnish counterpart of Pig Latin. This game is also called siansaksa ('Pig German'), 
+# which is a common expression for unintelligible gibberish.
+#
+# Sananmuunnos - Spoonerism: swap first morae of words. 
+# Apply vowel harmony (vokaalisointu) according to the initial syllable, 
+# repair "broken diphthongs" into permitted diphthongs
+# 
+# hyphenate - tavuta
+#
+# Lyle Campbell. 1980. The Psychological and Sociological Reality of Finnish Vowel Harmony. 
+# In Issues in Vowel Harmony, edited by Robert Vago, pp. 245-270. 
+# (Studies in Language Companion Series, 6.) John Benjamins.
 module Kontti
-  # Your code goes here...
+
+	AFFIX = "kontti"
+
+  class KonttiFun
+    
+    def change( input )
+    	add_konnti( input )
+    end
+    
+    def add_konnti( input )
+    	input + "-" + Kontti::AFFIX
+    end
+  end
 end
--- spec/kontti_spec.rb
+++ spec/kontti_spec.rb
@@ -1,8 +1,15 @@
 require 'kontti'
 
+# How to run these tests:
+# rspec spec/kontti_spec.rb 
 describe Kontti::KonttiFun do
   it "returns kovonen-hentti for hevonen" do
     k = Kontti::KonttiFun.new
     k.change( "hevonen" ).should eq("kovonen-hentti")
+  end
+
+  it "adds konnti after a word" do
+  	k = Kontti::KonttiFun.new
+  	k.add_konnti( "hevonen" ).should eq("hevonen-kontti")
   end
 end
